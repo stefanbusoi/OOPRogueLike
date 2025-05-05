41082c8a546776df12671ae4c49e8209f258eb19
stefanbusoi <stefan.busoi12@yahoo.com>
2025-05-05 21:00:19 +0300

    Changed path

--- CPP_Files/Weapons/Firearm.cpp
+++ CPP_Files/Weapons/Firearm.cpp
@@ -7,6 +7,6 @@
 Firearm::Firearm(const std::string &name, sf::Transform transform):GameObject(name,transform,nullptr) {
     sf::Transform tr=sf::Transform::Identity;
     tr.translate(sf::Vector2f(0.0f,40.0f)).scale(sf::Vector2f(100.f, -130.0f));
-    EmplaceGameObject<ShapeRenderer>("CircleRenderer",tr, std::filesystem::path("../Assets/bow_arrow.png"), RenderOrder::PostProcessing,GeometryShape::Square);
+    EmplaceGameObject<ShapeRenderer>("CircleRenderer",tr, std::filesystem::path("Assets/bow_arrow.png"), RenderOrder::PostProcessing,GeometryShape::Square);
 }
 
